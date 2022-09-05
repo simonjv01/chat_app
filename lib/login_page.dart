fef9bfee2ab9fbef49a5c709c2f03cb2fa0508d0
Simon Vargas <sjvargas01@gmail.com>
2022-09-05 16:52:39 -0400

    added image

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -17,17 +17,19 @@ class LoginPage extends StatelessWidget {
 
       body: Column(
 
-        children: const [
-          Text('Let\'s Sign you in!',
+        children: [
+          const Text('Let\'s Sign you in!',
             style: TextStyle(fontSize: 30,
                 color: Colors.brown,
                 fontWeight: FontWeight.bold,
                 letterSpacing: 0.5),
           ),
-          Text('Welcome back! \nYou\'ve been missed!',
+          const Text('Welcome back! \nYou\'ve been missed!',
           style: TextStyle(fontWeight: FontWeight.bold,
           fontSize: 20,
-          color: Colors.blue),),
+          color: Colors.blue),
+          ),
+          Image.network('https://3009709.youcanlearnit.net/Alien_LIL_131338.png')
         ],
       ),
     );
