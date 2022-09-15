c2f648dfd3c7d6951d5be7fff046a850e03477e7
Simon Vargas <sjvargas01@gmail.com>
2022-09-15 14:42:08 -0400

    refactoring chatpage

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -4,11 +4,44 @@ import 'package:flutter/material.dart';
 class ChatPage extends StatelessWidget {
   const ChatPage({Key? key}) : super(key: key);
 
+
+  Widget getChatBubble(alignment, message) {
+    return
+      Align(
+        alignment: alignment,
+        child: Container(
+          padding: const EdgeInsets.all(24),
+          margin: const EdgeInsets.all(50),
+          decoration: const BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(12),
+                topRight: Radius.circular(12),
+                bottomLeft: Radius.circular(12)
+            ),
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+               Text('$message',
+                  style: const TextStyle(fontSize: 20, color: Colors.white),
+              ),
+              Image.network(
+                'https://3009709.youcanlearnit.net/Alien_LIL_131338.png',
+                height: 100,)
+            ],
+          ),
+        ),
+      );
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        backgroundColor: Colors.white,
         title: const Text('Hi Simon'),
         actions: [
           IconButton(
@@ -21,102 +54,64 @@ class ChatPage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          ListView(
-            children: [
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Container(
-                  padding: const EdgeInsets.all(24),
-                  margin: const EdgeInsets.all(50),
-                  decoration: const BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(12),
-                        topRight: Radius.circular(12),
-                        bottomLeft: Radius.circular(12)
-                  ),
-                ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children:  [
-                      const Text('Hi, this is your message!',
-                        style: TextStyle(fontSize: 20, color: Colors.white)),
-                      Image.network('https://3009709.youcanlearnit.net/Alien_LIL_131338.png', height: 100,)
-                    ],
-                  ),
-    ),
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  padding: const EdgeInsets.all(24),
-                  margin: const EdgeInsets.all(50),
-                  decoration: const BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(12),
-                        topRight: Radius.circular(12),
-                        bottomLeft: Radius.circular(12)
-                    ),
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children:  [
-                      const Text('Hi, this is your message!',
-                          style: TextStyle(fontSize: 20, color: Colors.white)),
-                      Image.network('https://3009709.youcanlearnit.net/Alien_LIL_131338.png', height: 100,)
-                    ],
-                  ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  padding: const EdgeInsets.all(24),
-                  margin: const EdgeInsets.all(50),
-                  decoration: const BoxDecoration(
-                    color: Colors.grey,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(12),
-                        topRight: Radius.circular(12),
-                        bottomLeft: Radius.circular(12)
-                    ),
-                  ),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children:  [
-                      const Text('Hi, this is your message!',
-                          style: TextStyle(fontSize: 20, color: Colors.white)),
-                      Image.network('https://3009709.youcanlearnit.net/Alien_LIL_131338.png', height: 100,)
-                    ],
-                  ),
-                ),
-              ),
+          Expanded(
+            child: ListView(
+              children: [
 
-              Container(
-                height: 100,
-                decoration: const BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.vertical(top: Radius.circular(20) )
+
+    ),
                 ),
-                child: Row(
-                  children: [
-                    IconButton(
+
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: Container(
+                    padding: const EdgeInsets.all(24),
+                    margin: const EdgeInsets.all(50),
+                    decoration: const BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          topRight: Radius.circular(12),
+                          bottomLeft: Radius.circular(12)
+                      ),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children:  [
+                        const Text('Hi, this is your message!',
+                            style: TextStyle(fontSize: 20, color: Colors.white)),
+                        Image.network('https://3009709.youcanlearnit.net/Alien_LIL_131338.png', height: 100,)
+                      ],
+                    ),
+                  ),
+                ),
+
+                Container(
+                  height: 100,
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.vertical(top: Radius.circular(20) )
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: const Icon(Icons.add,
+                            color: Colors.white,
+                          ),
+                      ),
+                      IconButton(
                         onPressed: () {},
-                        icon: const Icon(Icons.add,
+                        icon: const Icon(Icons.send,
                           color: Colors.white,
                         ),
-                    ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.send,
-                        color: Colors.white,
-                      ),
-                    )],
-                ),
-              )
+                      )],
+                  ),
+                )
 
-              ],
+                ],
+            ),
           ),
         ],
       )
